a668322e14288a1a7f1bfad2328e7b98f31f3dc7
Maya Peters <mayampeters@gmail.com>
2023-04-28 15:35:11 -0500

    edited collectInfo function to use objects and changed strategy for appending to DOM

--- script.js
+++ script.js
@@ -4,27 +4,34 @@ let totalCosts = 0;
 
 function collectInfo(event) {
     event.preventDefault();
-    //Defining variables
-    let firstName = document.querySelector('#first-name').value;
-    let lastName = document.querySelector('#last-name').value;
-    let idNum = document.querySelector('#ID-number').value;
-    let jobTitle = document.querySelector('#job-title').value;
-    let annualSalary = document.querySelector('#annual-salary').value;
 
-    //Putting variables into an array
-    let arrayOfInfo = [];
-    arrayOfInfo.push(firstName, lastName, idNum, jobTitle, annualSalary);
-
-    //Appending variables to the DOM;
-    for (let item of arrayOfInfo) {
-        document.querySelector('#new-info').innerHTML += `<div>${item}</div>`
+    //Creating employee object
+    let newEmployee = {
+        firstName: document.querySelector('#first-name').value,
+        lastName: document.querySelector('#last-name').value,
+        idNum: document.querySelector('#ID-number').value,
+        jobTitle: document.querySelector('#job-title').value,
+        annualSalary: document.querySelector('#annual-salary').value
     }
 
-    //Displaying total costs
-    totalCosts += Number(annualSalary);
+    console.log(newEmployee);
+
+    //Appending to the DOM
+    document.querySelector('#new-info').innerHTML += 
+    `<div>
+        <span>${newEmployee.firstName}</span>
+        <span>${newEmployee.lastName}</span>
+        <span>${newEmployee.idNum}</span>
+        <span>${newEmployee.jobTitle}</span>
+        <span>$${newEmployee.annualSalary}</span>
+        <button onClick="deleteMe(event)">Delete</button>
+    </div>`
+
+    // Displaying total costs
+    totalCosts += Number(newEmployee.annualSalary);
     document.querySelector('#total-costs').innerHTML = `Total Costs: $${totalCosts}`;
 
-    if(totalCosts > 10000) {
+    if(totalCosts > 20000) {
         document.querySelector('#total-costs').style.backgroundColor = 'red';
     }
 
