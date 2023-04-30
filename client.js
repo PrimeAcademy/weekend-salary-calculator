89768c56db983142968aa101233ff89a3a1bb023
Hannah Engelhart <hannah.engelhart@gmail.com>
2023-04-30 16:22:05 -0500

    added table and adjusted delete button

--- client.js
+++ client.js
@@ -1,1 +1,34 @@
 console.log('Hello World')
+
+let currentIndex = -1;
+
+function submitEmployee(event) {
+currentIndex += 1;
+    event.preventDefault();
+
+    let firstNameVal = document.querySelector('#first-name').value;
+    let lastNameVal = document.querySelector('#last-name').value;
+    let IDVal = document.querySelector('#ID').value;
+    let titleVal = document.querySelector('#title').value;
+    let annualSalaryVal = document.querySelector('#annual-salary').value;
+
+
+    let employeeTable = document.querySelector('#employeeData');
+
+    employeeTable.innerHTML += `
+    <tr>
+        <td>${currentIndex}</td>
+        <td>${firstNameVal}</td>
+        <td>${lastNameVal}</td>
+        <td>${IDVal}</td>
+        <td>${titleVal}</td>
+        <td>${annualSalaryVal}</td>
+        <td><button onclick="deleteEmployee(event)" class="delete-employee">Delete</button></td>
+    </tr>
+    `
+}
+
+
+function deleteEmployee(currentIndex) {
+    document.getElementById("employeeData").deleteRow(currentIndex);
+}
