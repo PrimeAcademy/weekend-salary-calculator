3d25416ca8bfba3b03c0ce6f91e158d5effad2a0
Hannah Engelhart <hannah.engelhart@gmail.com>
2023-05-01 20:36:38 -0500

    displayed annual salary as currency

--- client.js
+++ client.js
@@ -9,10 +9,15 @@ function submitEmployee(event) {
     let lastNameVal = document.querySelector('#last-name').value;
     let IDVal = document.querySelector('#ID').value;
     let titleVal = document.querySelector('#title').value;
+
+    let USDollar = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    })
     let annualSalaryVal = parseFloat(document.querySelector('#annual-salary').value);
 
-
     let employeeTable = document.querySelector('#employeeData');
+   
 
     employeeTable.innerHTML += `
     <tr>
@@ -20,7 +25,7 @@ function submitEmployee(event) {
         <td>${lastNameVal}</td>
         <td>${IDVal}</td>
         <td>${titleVal}</td>
-        <td class='total'>${annualSalaryVal}</td>
+        <td class='total'>${annualSalaryVal.toLocaleString("en-US", {style:"currency", currency:"USD"})}</td>
         <td><button onclick="deleteEmployee(this)" class="delete-employee">Delete</button></td>
     </tr>
     `
@@ -48,16 +53,29 @@ function totalMonthly() {
     let array = document.getElementsByClassName("total");
     for (let row of array) {
         monthlyCost += parseFloat(row.textContent);
+      
     }
-    document.querySelector('#total-monthly').innerHTML = monthlyCost;
-
- 
+   let element = document.querySelector('#total-monthly');
+    if (monthlyCost <= 20000) {
+        element.classList.remove('red-background')    
+    }
+    else {
+        element.classList.add('red-background')
+    }
+    let USDollar = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    })
+    element.innerHTML = USDollar.format(monthlyCost);
+    
 }
 
-// add element for total monthly cost
-// calculate monthly costs from all employees - run when submitted
+
+
+// X add element for total monthly cost
+// X calculate monthly costs from all employees - run when submitted
 // change background of monthly cost to red if total cost exceeds $20k
 // clear input fields upon submit - each input is an object, target element set value to empty string
 // make 'enter' submit input field - wrap input/labels in a form element 'submit' and change 'onclick' to 'onsubmit'
 // STRETCH add styling
-// STRETCH when employee is deleted updated total monthly cost to reflect - similar to finding total - on delete run a function to re-tally total
+// X STRETCH when employee is deleted updated total monthly cost to reflect - similar to finding total - on delete run a function to re-tally total
